da362230d68b303be86eb73c9397a44442e05c2d
BDang029 <dodang159@gmail.com>
2024-05-22 19:13:20 +0700

    Update User Information

--- app/src/main/java/com/example/foody_android/callAPI/RetrofitInterface.java
+++ app/src/main/java/com/example/foody_android/callAPI/RetrofitInterface.java
@@ -1,11 +1,14 @@
 package com.example.foody_android.callAPI;
 
 import com.example.foody_android.model.LoginResult;
+import com.example.foody_android.model.User;
 
 import java.util.HashMap;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.POST;
 public interface RetrofitInterface {
     @POST("/api/auth/signin")
@@ -14,7 +17,10 @@ public interface RetrofitInterface {
     Call<LoginResult> executeSignup (@Body HashMap<String, String> map);
     @POST("/api/user/otp/verify-otp")
     Call<LoginResult> verifyOtp(@Body HashMap<String, String> map);
-
     @POST("/api/user/otp/send-otp")
     Call<LoginResult> sendOTP(@Body HashMap<String, String> map);
+
+    @GET("/api/test/user")
+    Call<User> getUserProfile(@Header("Authorization") String authToken);
+
 }
---
+++ app/src/main/java/com/example/foody_android/model/User.java
@@ -0,0 +1,27 @@
+package com.example.foody_android.model;
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    private LoginResult loginResult;
+
+    private Object userProfile;
+
+    public LoginResult getLoginResult(){
+        return loginResult;
+    }
+
+    public void setLoginResult(LoginResult loginResult) {
+        this.loginResult = loginResult;
+    }
+
+    public Object getUserProfile() {
+        return userProfile;
+    }
+
+    public void setUserProfile(Object userProfile) {
+        this.userProfile = userProfile;
+    }
+
+
+}
